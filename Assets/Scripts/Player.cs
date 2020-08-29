292e0eda69ca66081c439184a18f3e2c699f42b0
Dima <dimasadidas1488@gmail.com>
2020-08-29 17:47:02 +0300

    Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     public float Speed;
     public float XIncrement;
     public float MinWidth, MaxWidth;
+    public Animator _animator;
 
 
     private Vector2 _targetPos = Vector2.zero;
@@ -23,10 +24,17 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D) && _targetPos.x < MaxWidth)
         {
             _targetPos = new Vector2(_targetPos.x + XIncrement, _targetPos.y);
+            _animator.SetBool("Right", true);
         }
         else if (Input.GetKeyDown(KeyCode.A) && _targetPos.x > MinWidth)
         {
             _targetPos = new Vector2(_targetPos.x - XIncrement, _targetPos.y);
+            _animator.SetBool("Left", true);
+        }
+        else if (Vector3.Distance(transform.position, _targetPos) <0.1f)
+        {
+            _animator.SetBool("Right", false);
+            _animator.SetBool("Left", false);
         }
 
     }
