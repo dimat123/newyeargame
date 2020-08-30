734d0060efc020fa6f8968adaec9434cd10887b5
jesusamex <romanmalinovsk20004@gmail.com>
2020-08-30 20:02:25 +0300

    добавив включення світла

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+
+    public GameObject Sviet;
+    
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        Debug.Log("Smth touched");
+        Sviet.SetActive(true);
+    }
+}
