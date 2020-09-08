c0910abe756c28f76e0922054e497c3edbf1b495
jesusamex <romanmalinovsk20004@gmail.com>
2020-09-08 21:06:18 +0300

    ...

--- Assets/Scripts/Swipes.cs
+++ Assets/Scripts/Swipes.cs
@@ -1,15 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Swipes : MonoBehaviour
 {
-
-    float dir = 1;
-    public float speed;
-    public float jumpHeight;
-    private Rigidbody2D playerRB;
-    bool facingRight = true;
+    public Text SidesMove;
 
     public float maxSwipetime;
     public float minSwipeDistance;
@@ -22,42 +18,10 @@ public class Swipes : MonoBehaviour
     private Vector2 endSwipePosition;
     private float swipeLegth;
 
-    void Start()
-    {
-        playerRB = GetComponent<Rigidbody2D>();
-    }
-
     // Update is called once per frame
     void Update()
     {
-        playerRB.velocity = new Vector2(dir * speed * Time.deltaTime, playerRB.velocity.y);
-        PlayerController();
-        PlayerJump();
         SwipeTest();
-    }
-    void PlayerController()
-    {
-        if (Input.GetKeyDown(KeyCode.A) && facingRight == true)
-        {
-            FlipAndMove();
-        }
-        else if (Input.GetKeyDown(KeyCode.D) && facingRight == false)
-        {
-            FlipAndMove();
-        }
-    }
-    void PlayerJump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            playerRB.velocity = Vector2.up * jumpHeight * Time.deltaTime;
-        }
-    }
-    void FlipAndMove()
-    {
-        dir = -dir;
-        transform.Rotate(0, 180, 0);
-        facingRight = !facingRight;
     }
     void SwipeTest()
     {
@@ -84,32 +48,16 @@ public class Swipes : MonoBehaviour
 
         }
     }
-        void swipeControl()
+    void swipeControl()
+    {
+        Vector2 Distance = endSwipePosition - startSwipePosition;
+        if (Distance.x > 0)
         {
-            Vector2 Distance = endSwipePosition - startSwipePosition;
-            float xDistance = Mathf.Abs(Distance.x);
-            float yDistance = Mathf.Abs(Distance.y);
-            if (xDistance > yDistance)
-            {
-                if (Distance.x > 0 && facingRight)
-                {
-                    FlipAndMove();
-                }
-                else if (Distance.x < 0 && facingRight)
-                {
-                    FlipAndMove();
-                }
-            }
-            else if (yDistance > xDistance)
-            {
-                if (Distance.y > 0)
-                {
-                    playerRB.velocity = Vector2.up * jumpHeight * Time.deltaTime;
-                }
-                else if (Distance.y < 0)
-                {
-                    Debug.Log("SwipeRight");
-                }
-            }
+            SidesMove.text = "R";
+        }
+        else if (Distance.x < 0)
+        {
+            SidesMove.text = "L";
         }
     }
+}
