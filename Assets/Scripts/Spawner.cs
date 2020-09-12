ac52ef6e3fd4d673d6b6d8821af611803aba13a8
jesusamex <romanmalinovsk20004@gmail.com>
2020-09-12 21:32:30 +0300

    спавнер

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public Transform SpawnPos;
+    public GameObject House;
+    public float TimeSpawn;
+
+    void Start()
+    {
+        StartCoroutine(SpawnCD());
+    }
+    void Repeat()
+    {
+        StartCoroutine(SpawnCD());
+    }
+    IEnumerator SpawnCD()
+    {
+        yield return new WaitForSeconds (TimeSpawn);
+        Instantiate(House, SpawnPos.position, Quaternion.identity);
+        Repeat();
+    }
+}
